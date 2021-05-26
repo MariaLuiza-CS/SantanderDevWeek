54c73be7c7c99c7623eaafb62fc08414b5f3991c
MariaLuiza-CS <m.luiza1843@gmail.com>
2021-05-25 22:19:24 -0300

    Adicionando primeiro dia 01 do projeto

---
+++ app/src/main/java/com/example/santanderdevweek/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.santanderdevweek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
+import kotlin.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

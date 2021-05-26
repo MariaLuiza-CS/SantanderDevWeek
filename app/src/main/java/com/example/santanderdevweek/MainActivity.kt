08ad4a11e98e4ab56bae286ac180f98a07bbf052
MariaLuiza-CS <m.luiza1843@gmail.com>
2021-05-26 19:54:06 -0300

    Adicionando primeiro dia 02 do projeto

--- app/src/main/java/com/example/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/example/santanderdevweek/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBar
 import kotlin.Exception
@@ -11,5 +14,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu_notification, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item03 -> {
+                Log.d("CLICK", "click item 02")
+                true
+            }
+            else ->super.onOptionsItemSelected(item)
+        }
     }
 }
